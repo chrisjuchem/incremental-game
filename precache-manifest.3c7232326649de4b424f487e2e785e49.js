b73785ffb13c47ec0213119b016d43751c027766
chrisjuchem <chrisjuchem15@gmail.com>
2020-05-03 13:04:04 -0400

    Updates

---
+++ precache-manifest.3c7232326649de4b424f487e2e785e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e5375ceb983f20744a6178826d5e64",
+    "url": "/incremental-game/index.html"
+  },
+  {
+    "revision": "f201edc40931f149aef4",
+    "url": "/incremental-game/static/css/main.830ce905.chunk.css"
+  },
+  {
+    "revision": "23f1cbafc2c440a636da",
+    "url": "/incremental-game/static/js/2.fa65d022.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/incremental-game/static/js/2.fa65d022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f201edc40931f149aef4",
+    "url": "/incremental-game/static/js/main.5201b757.chunk.js"
+  },
+  {
+    "revision": "5b82fb644825dee53a07",
+    "url": "/incremental-game/static/js/runtime-main.90a88586.js"
+  }
+]);
