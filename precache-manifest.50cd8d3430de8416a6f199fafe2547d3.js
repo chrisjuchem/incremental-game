5d5a3d9e62dcd5eb94edcc63bec68fcb206f5046
chrisjuchem <chrisjuchem15@gmail.com>
2020-05-03 13:02:24 -0400

    Updates

---
+++ precache-manifest.50cd8d3430de8416a6f199fafe2547d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9cfc807a04bf718d5432e1a157ed7b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f22dfbe3139dfbe2a36f",
+    "url": "/static/css/main.830ce905.chunk.css"
+  },
+  {
+    "revision": "23f1cbafc2c440a636da",
+    "url": "/static/js/2.fa65d022.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.fa65d022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22dfbe3139dfbe2a36f",
+    "url": "/static/js/main.38b51ff1.chunk.js"
+  },
+  {
+    "revision": "c6e700f1dec74b6f89bc",
+    "url": "/static/js/runtime-main.4428e074.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/incremental-game/precache-manifest.3c7232326649de4b424f487e2e785e49.js"
+  "/precache-manifest.50cd8d3430de8416a6f199fafe2547d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/incremental-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
