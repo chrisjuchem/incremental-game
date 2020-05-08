1292517e7be8218eebaf06d5386d2b9d3ced18bf
Noah Lichtenstein <lichto2005@gmail.com>
2020-05-07 23:20:16 -0400

    Updates

---
+++ precache-manifest.e23c46a514221cd24be8ccd2ff14b3e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aee60841a11a233210e00b53dbb4899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c194995079af5831200",
+    "url": "/static/css/2.23bb4cad.chunk.css"
+  },
+  {
+    "revision": "02db098a68c732cef61d",
+    "url": "/static/css/main.23a594a2.chunk.css"
+  },
+  {
+    "revision": "7c194995079af5831200",
+    "url": "/static/js/2.3294dcf9.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.3294dcf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02db098a68c732cef61d",
+    "url": "/static/js/main.353d2277.chunk.js"
+  },
+  {
+    "revision": "c6e700f1dec74b6f89bc",
+    "url": "/static/js/runtime-main.4428e074.js"
+  }
+]);
