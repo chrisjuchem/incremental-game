d29bc300cff77bfe59d5cb5d7d8b8841a82e9a3a
chrisjuchem <chrisjuchem15@gmail.com>
2020-05-08 23:49:07 -0400

    perspective but needs nome number tweaks

--- src/ui/map/factoryMapContainer.js
+++ src/ui/map/factoryMapContainer.js
@@ -6,8 +6,12 @@ function FactoryMapContainer() {
     const [dragging, setDragging] = useState(false);
     const [offset, setOffset] = useState({x:0, y:0});
 
-    const maxX = 200;
-    const maxY = 200;
+    const innerMapX = 1200;
+    const innerMapY = 800;
+    const outerBoundsX = 900;
+    const outerBoundsY = 600
+    const maxX = innerMapX - outerBoundsX;
+    const maxY = innerMapY - outerBoundsY;
 
     const clickHandler = (event) => {
         if (event.button === 0){
@@ -41,10 +45,22 @@ function FactoryMapContainer() {
         }
     }
 
+    return <div className='mapContainer'
+                style={{
+                    width: `${outerBoundsX}px`,
+                    height: `${outerBoundsY}px`,
+                }}>
+        <div className="innerMapContainer"
+             style={{
+                left: `${offset.x}px`,
+                top:`${offset.y}px`,
 
-    return <div className='mapContainer'>
-        <div className="innerMapContainer" style={{left: `${offset.x}px`, top:`${offset.y}px`}}
-            onMouseDown={clickHandler} onMouseMove={dragHandler} onMouseUp={releaseHandler}>
+                width: `${innerMapX}px`,
+                height: `${innerMapY}px`,
+             }}
+             onMouseDown={clickHandler}
+             onMouseMove={dragHandler}
+             onMouseUp={releaseHandler}>
             <FactoryMap/>
         </div>
     </div>
